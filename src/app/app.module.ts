f1de601ee6e499a406f188131201047316968780
Edson Andrade <pereira.edson@ctis.com.br>
2018-03-27 13:54:32 -0300

    Adicionado até a aula 13.4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 
 import { InputTextModule } from 'primeng/components/inputtext/inputtext';
 import { ButtonModule } from 'primeng/components/button/button';
-
+import {InputTextareaModule} from 'primeng/inputtextarea';
 import {DataTableModule} from 'primeng/datatable';
-
 import {TooltipModule} from 'primeng/tooltip';
 
 import { AppComponent } from './app.component';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+import { LancamentosCadastroComponent } from './lancamentos-cadastro/lancamentos-cadastro.component';
+import { BrowserModule } from '@angular/platform-browser';
+import {CalendarModule} from 'primeng/calendar';
+import {SelectButtonModule} from 'primeng/selectbutton';
+import {DropdownModule} from 'primeng/dropdown';
+
 
 
 @NgModule({
@@ -19,10 +25,20 @@ import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.co
     AppComponent,
     LancamentosPesquisaComponent,
     NavbarComponent,
-    PessoasPesquisaComponent
+    PessoasPesquisaComponent,
+    LancamentosCadastroComponent
   ],
   imports: [
-    BrowserModule, InputTextModule, ButtonModule, DataTableModule, TooltipModule
+    BrowserModule,
+    InputTextModule,
+    ButtonModule,
+    DataTableModule,
+    TooltipModule,
+    InputTextareaModule,
+    CalendarModule,
+    BrowserAnimationsModule,
+    SelectButtonModule,
+    DropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
